86fec3d6a4982ce982b3ac16a09e80ced559e3af
Jacob VanDomelen <Jacob.Vandome15@gmail.com>
2020-06-25 19:47:37 -0700

    Adding virtual environment installation.

--- PaletteGenerator.py
+++ PaletteGenerator.py
@@ -1,4 +1,13 @@
 import json
+import matplotlib.pyplot as plt
+import numpy as py
+
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+print(tf.__version__)
 
 with open('data.json', 'r') as file:
     file_data = file.read()
@@ -8,4 +17,4 @@ json_data = json.loads(file_data)
 # cleaning and preparing the data
 json_data = [row[0:4] for row in json_data if len(row) >= 4]
 
-print(json.dumps(json_data, indent=4))
+# print(json.dumps(json_data, indent=4))
