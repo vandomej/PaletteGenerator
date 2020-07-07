4cf16e36a891922794bd98b577b7ccd7ef57ee58
Jacob VanDomelen <Jacob.Vandome15@gmail.com>
2020-07-06 23:11:44 -0700

    Trying to load tf model into javascript.

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import { PageProps } from 'gatsby';
+import * as tf from '@tensorflow/tfjs';
 
 import Title from '@/components/Title';
+import model from '../model/model.json';
 
-const Home: React.FC<PageProps> = () => (
-  <main>
-    <Title />
-    <p>A TypeScript starter for Gatsby. Great for advanced users.</p>
-    <p>
-      Follow me on Twitter (
-      <a href="https://twitter.com/jpedroschmitz">@jpedroschmitz</a>)
-    </p>
-  </main>
-);
+const Home: React.FC<PageProps> = () => {
+  tf.loadLayersModel(model.toString()).then((m) => {
+    const prediction = m.predict(tf.tensor1d([0.75, 0.5, 0.25], 'float32'));
+    console.log(prediction);
+  });
+
+  return (
+    <main>
+      <Title />
+      <p>A TypeScript starter for Gatsby. Great for advanced users.</p>
+      <p>
+        Follow me on Twitter (
+        <a href="https://twitter.com/jpedroschmitz">@jpedroschmitz</a>)
+      </p>
+    </main>
+  );
+};
 
 export default Home;
