87fd9458ae51c58b10cee57c18b8faf010001258
Jacob VanDomelen <Jacob.Vandome15@gmail.com>
2020-07-08 22:06:29 -0700

    Getting develop to work but build is busted shit.

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -1,5 +1,14 @@
 module.exports = {
   // Since `gatsby-plugin-typescript` is automatically included in Gatsby you
   // don't need to define it here (just if you need to change the options)
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/static/`,
+        ignore: [`**/\.*`], // ignore files starting with a dot
+      },
+    },
+  ],
 };
--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import { PageProps } from 'gatsby';
-import * as tf from '@tensorflow/tfjs';
 
-import Title from '@/components/Title';
-import model from '../../public/model.json';
+import loadable from '@loadable/component';
+
+const Title = loadable(() => import('../components/Title'));
 
 const Home: React.FC<PageProps> = () => {
-  tf.loadLayersModel('http://localhost:8000/model.json').then((m) => {
-    const prediction = m.predict(tf.tensor([[0.75, 0.5, 0.25]]));
-    (prediction as tf.Tensor).print();
-  });
-
   return (
     <main>
       <Title />
